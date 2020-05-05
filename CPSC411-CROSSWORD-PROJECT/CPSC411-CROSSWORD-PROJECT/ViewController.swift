1b859f04b35778e4b8deccc1afcf923dd8004c40
Victoria  Tran <victoria.tran@csu.fullerton.edu>
2020-05-04 19:29:08 -0700

    Add files via upload

---
+++ CPSC411-CROSSWORD-PROJECT/CPSC411-CROSSWORD-PROJECT/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CPSC411-CROSSWORD-PROJECT
+//
+//  Created by Victoria Tran on 5/4/20.
+//  Copyright © 2020 Victoria Tran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CPSC411-CROSSWORD-PROJECT/CPSC411-CROSSWORD-PROJECTTests/CPSC411_CROSSWORD_PROJECTTests.swift
@@ -0,0 +1,34 @@
+//
+//  CPSC411_CROSSWORD_PROJECTTests.swift
+//  CPSC411-CROSSWORD-PROJECTTests
+//
+//  Created by Victoria Tran on 5/4/20.
+//  Copyright © 2020 Victoria Tran. All rights reserved.
+//
+
+import XCTest
+@testable import CPSC411_CROSSWORD_PROJECT
+
+class CPSC411_CROSSWORD_PROJECTTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
